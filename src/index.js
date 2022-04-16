74c61819c95f9aeeba5bfb6842966e46be28ff19
Amanda Nobre <34144662+AmandaNobre@users.noreply.github.com>
2022-04-16 20:51:22 -0300

    Criando rotas

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const { request } = require("express");
+const { response } = require("express");
+const express = require("express");
+
+const app = express();
+
+app.get("/cursos", (request, response) => {
+  return response.json(["curso1", "curso2", "curso3"]);
+});
+
+app.post("/cursos", (request, response) => {
+  return response.json(["curso1", "curso2", "curso3", "curso4"]);
+});
+
+app.put("/cursos/:id", (request, response) => {
+  return response.json(["curso5", "curso2", "curso3", "curso4"]);
+});
+
+app.patch("/cursos/:id", (request, responde) => {
+  return response.json(["curso5", "curso7", "curso3", "curso4"]);
+});
+
+app.delete("/cursos/:id", (request, response) => {
+  return response.json(["curso5", "curso2", "curso4"]);
+});
+
+//Porta que inicia o projeto
+app.listen(3333);
