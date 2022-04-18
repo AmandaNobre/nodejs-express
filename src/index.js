c4319b5f3df27277e8a30a0535d5a913885f6e63
Amanda Nobre <34144662+AmandaNobre@users.noreply.github.com>
2022-04-18 18:40:52 -0300

    Criando conta e verificando se cpf já existe

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 const express = require("express");
+const { v4: uuidV4 } = require("uuid");
 
 const app = express();
-app.use(express.json);
+app.use(express.json());
 
-// request.params retorna os parâmetros
-// request,query retorna os query, serve para paginação ou filtro
-// request.body retorna o body
+const contas = [];
 
-app.get("/cursos", (request, response) => {
-  return response.json(["curso1", "curso2", "curso3"]);
-});
+//cadastrar conta
+// confirm, nome, id, statement
+app.post("/conta", (request, response) => {
+  const { cpf, nome } = request.body;
 
-app.post("/cursos", (request, response) => {
-  return response.json(["curso1", "curso2", "curso3", "curso4"]);
-});
+  const cpfExiste = contas.some((contas) => contas.cpf === cpf);
 
-app.put("/cursos/:id", (request, response) => {
-  return response.json(["curso5", "curso2", "curso3", "curso4"]);
-});
+  if (cpfExiste) {
+    return response.status(400).json({ error: "Cpf já existente" });
+  }
 
-app.patch("/cursos/:id", (request, response) => {
-  return response.json(["curso5", "curso7", "curso3", "curso4"]);
-});
+  contas.push({
+    id: uuidV4(),
+    cpf,
+    nome,
+    acoes: [],
+  });
 
-app.delete("/cursos/:id", (request, response) => {
-  return response.json(["curso5", "curso2", "curso4"]);
+  return response.status(201).send();
 });
 
 app.listen(3333);
