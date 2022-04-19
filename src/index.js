7f7257840610780042b9cfafbf7c2a64d8f1e0cc
AmandaNobre <amanda.silva@ajaxti.com.br>
2022-04-19 15:41:09 -0300

    Finalizando projeto

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const contas = [];
 
 // Midleware
 function verifyIfExistsAccount(request, response, next) {
-  const { cpf } = request.params;
+  const { cpf } = request.headers;
   const conta = contas.find((conta) => conta.cpf === cpf);
 
   if (!conta) {
@@ -40,11 +40,62 @@ app.post("/conta", (request, response) => {
   return response.status(201).send();
 });
 
+//Inserir deposito
+app.post("/deposito", verifyIfExistsAccount, (request, response) => {
+  const { descricao, valor } = request.body;
+
+  const { conta } = request
+
+  const operacao = {
+    descricao,
+    valor,
+    dataCriacao: new Date(),
+    tipo: "credito"
+  }
+
+  conta.acoes.push(operacao)
+
+  return response.status(200).send()
+})
+
 // buscar extrato bancario
-app.get("/extrato/:cpf", verifyIfExistsAccount, (request, response) => {
+app.get("/extrato", verifyIfExistsAccount, (request, response) => {
   const { conta } = request
   return response.json(conta.acoes);
 });
+
+// buscar extrato bancario por data
+app.get("/extrato/data", verifyIfExistsAccount, (request, response) => {
+  const { conta } = request
+  const { data } = request.query
+
+  const dataFormat = new Date(data + " 00:00")
+  const contaFiltro = conta.acoes.filter(conta => conta.dataCriacao.toDataString() === new Date((dataFormat).toDateString()))
+  return response.json(contaFiltro);
+});
+
+//buscar conta
+app.get("/conta", verifyIfExistsAccount, (request, response) => {
+  const { conta } = request
+  return response.json(conta)
+})
+
+//editar conta
+app.put("/conta", verifyIfExistsAccount, (request, response) => {
+  const { nome } = request.body
+  const { conta } = request
+
+  conta.nome = nome
+
+  return response.status(201).send()
+})
+
+//deletar conta
+app.delete("/conta", verifyIfExistsAccount, (request, response) => {
+  const [conta] = request
+  contas.slice(conta, 1)
+  response.status(204)
+})
 
 // todas as rotas usam o Midleware
 // app.use(verifyIfExistsAccount)
